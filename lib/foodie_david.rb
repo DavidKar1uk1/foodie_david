74d40c55e826ebcb6db5d9ad21bcf6f168a724dd
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-14 10:53:28 +0300

    Created the Foodie_David Gem.

---
+++ lib/foodie_david.rb
@@ -0,0 +1,6 @@
+require "foodie_david/version"
+
+module FoodieDavid
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/foodie_david/version.rb
@@ -0,0 +1,3 @@
+module FoodieDavid
+  VERSION = "0.1.0"
+end
